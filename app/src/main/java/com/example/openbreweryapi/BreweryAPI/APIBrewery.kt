00857ce51bc730e02b7614e686e69b1b80e5571a
Leila Macatula <leilakriamacatula07@gmail.com>
2022-11-25 20:48:01 +0800

    First Commit

---
+++ app/src/main/java/com/example/openbreweryapi/BreweryAPI/APIBrewery.kt
@@ -0,0 +1,3 @@
+package com.example.openbreweryapi.BreweryAPI
+
+class APIBrewery : ArrayList<APIBreweryItem>()
---
+++ app/src/main/java/com/example/openbreweryapi/BreweryAPI/APIBreweryItem.kt
@@ -0,0 +1,21 @@
+package com.example.openbreweryapi.BreweryAPI
+
+data class APIBreweryItem(
+    var address_2: Any,
+    var address_3: Any,
+    var brewery_type: String,
+    var city: String,
+    var country: String,
+    var county_province: Any,
+    var created_at: String,
+    var id: String,
+    var latitude: String,
+    var longitude: String,
+    var name: String,
+    var phone: String,
+    var postal_code: String,
+    var state: String,
+    var street: String,
+    var updated_at: String,
+    var website_url: String
+)
